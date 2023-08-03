f5e41298bece02541e6a7699a735ee3618409f61
Nagesh Joshi <nagesh.joshi@rapidcircle.com>
2023-08-03 20:19:23 +0530

    GITHUB_TOKEN

--- Get-LookupTableOutput.ps1
+++ Get-LookupTableOutput.ps1
@@ -11,30 +11,31 @@ $env:GITHUB_REPOSITORY
 $env:GITHUB_RUN_ID
 $env:GITHUB_JOB
 
+$token
 $Context
 
 $env:GITHUB_CONTEXT
 
 #echo "::set-output name=LookupValue::$TableName"
 echo "LookupValue=SHELL01" >> $env:GITHUB_OUTPUT
-Write-Host "TableName: $($TableName)"
-Write-Host "Query: $($Query)"
-Write-Host "ColumnName: $($Column)"
-$TableName
-$Query
-$Column
+# Write-Host "TableName: $($TableName)"
+# Write-Host "Query: $($Query)"
+# Write-Host "ColumnName: $($Column)"
+# $TableName
+# $Query
+# $Column
 
-echo "LookupValue=SHELL02" >> $GITHUB_OUTPUT
+$env
 
-try {
-    $request = Invoke-RestMethod -Method POST `
-        -Uri "https://login.microsoftonline.com/$TenantName/oauth2/token"`
-        -Body @{ resource = $resourceAppIdURI; grant_type = "client_credentials"; client_id = $clientId; client_secret = $secret }`
-        -ContentType "application/x-www-form-urlencoded"
-    $access_token = $request.access_token
-    $access_token
-}
-catch {
-    $_
-    $_ >> $env:GITHUB_OUTPUT
-}
+# try {
+#     $request = Invoke-RestMethod -Method POST `
+#         -Uri "https://login.microsoftonline.com/$TenantName/oauth2/token"`
+#         -Body @{ resource = $resourceAppIdURI; grant_type = "client_credentials"; client_id = $clientId; client_secret = $secret }`
+#         -ContentType "application/x-www-form-urlencoded"
+#     $access_token = $request.access_token
+#     $access_token
+# }
+# catch {
+#     $_
+#     $_ >> $env:GITHUB_OUTPUT
+# }

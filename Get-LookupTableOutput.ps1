5d19139908912711812e3d4f7a46e8846ed681c1
Nagesh Joshi <nagesh.joshi@rapidcircle.com>
2023-08-03 17:58:43 +0530

    GitHub_Context param added to action

--- Get-LookupTableOutput.ps1
+++ Get-LookupTableOutput.ps1
@@ -12,8 +12,8 @@ $env:GITHUB_RUN_ID
 $env:GITHUB_JOB
 
 $Context
-$repo = $env:github.action_repository
-$repo
+
+$env:job
 
 #echo "::set-output name=LookupValue::$TableName"
 echo "LookupValue=SHELL01" >> $env:GITHUB_OUTPUT

f9fabb099dd351678027966ed32c786fd02238cd
Nagesh Joshi <nagesh.joshi@rapidcircle.com>
2023-08-03 13:31:59 +0530

    Added env variable to action

--- Get-LookupTableOutput.ps1
+++ Get-LookupTableOutput.ps1
@@ -4,6 +4,10 @@ param(
     [string] $Column
 )
 
+$TenantId
+$env.TenantId
+$ClientId
+$ClientSecret
 #echo "::set-output name=LookupValue::$TableName"
 echo "LookupValue=SHELL01" >> $env:GITHUB_OUTPUT
 Write-Host "TableName: $($TableName)"
@@ -14,3 +18,16 @@ $Query
 $Column
 
 echo "LookupValue=SHELL02" >> $GITHUB_OUTPUT
+
+try {
+    $request = Invoke-RestMethod -Method POST `
+        -Uri "https://login.microsoftonline.com/$TenantName/oauth2/token"`
+        -Body @{ resource = $resourceAppIdURI; grant_type = "client_credentials"; client_id = $clientId; client_secret = $secret }`
+        -ContentType "application/x-www-form-urlencoded"
+    $access_token = $request.access_token
+    $access_token
+}
+catch {
+    $_
+    $_ >> $env:GITHUB_OUTPUT
+}

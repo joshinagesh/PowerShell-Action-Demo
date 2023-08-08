e3244d36740bf83721960082878399ca362bd360
Nagesh Joshi <nagesh.joshi@rapidcircle.com>
2023-08-08 16:32:06 +0530

    Changed script file path

--- Actions/Get-LookupTableOutput/Get-LookupTableOutput.ps1
+++ Actions/Get-LookupTableOutput/Get-LookupTableOutput.ps1
@@ -1,24 +1,90 @@
 param(
     [string] $TableName,
     [string] $Query,
-    [string] $Column
+    [string] $Column,
+    [string] $GitHubToken,
+    [string] $TenantId,
+    [string] $ClientId,
+    [string] $ClientSecret,
+    [string] $LookupServiceIdentifier
 )
 
-Write-Host "TableName: $($TableName)"
-Write-Host "Query: $($Query)"
-Write-Host "ColumnName: $($Column)"
+$GitHubToken
+
 $TableName
-$Query
 $Column
+$Query
 
-$TenantName = "3154f54d-54c8-4ced-ac8d-1535d6d75ace"
-$resourceAppIdURI = "a6b7b60c-001b-42dd-9b0e-c89ad1cf699c"
-$clientId = "a6b7b60c-001b-42dd-9b0e-c89ad1cf699c"
-$secret = "Ze58Q~PEyb4uJjJPSJCK7gcoZFek2n2vAvxvEaOs"
+$env:GITHUB_ENV
+$env:GITHUB_ACTION_PATH
+$env:GITHUB_REPOSITORY
+$env:GITHUB_RUN_ID
+$env:GITHUB_JOB
 
-$request = Invoke-RestMethod -Method POST `
-    -Uri "https://login.microsoftonline.com/$TenantName/oauth2/token"`
-    -Body @{ resource = $resourceAppIdURI; grant_type = "client_credentials"; client_id = $clientId; client_secret = $secret }`
-    -ContentType "application/x-www-form-urlencoded"
-$access_token = $request.access_token
-$access_token
+$GitHubToken
+
+
+$env:LookupResourceId
+try {
+    $request = Invoke-RestMethod -Method POST `
+        -Uri "https://login.microsoftonline.com/$TenantId/oauth2/token" `
+        -Body @{ resource = $env:LookupResourceId; grant_type = "client_credentials"; client_id = $ClientId; client_secret = $ClientSecret }`
+        -ContentType "application/x-www-form-urlencoded"
+    $access_token = $request.access_token
+    $access_token
+}
+catch {
+    $_
+}
+
+
+
+try {
+    $Uri = "https://staging-dna-lkup-api.azurewebsites.net/api/Lookup/odata/$($TableName)?`$filter=$Query"
+    $Uri
+    $reqHeaders = @{
+        "Authorization" = "Bearer $($access_token)";
+        "Accept"        = "application/json";
+    }
+
+    $body
+    $res = Invoke-RestMethod -Method GET -Uri $Uri -Headers $reqHeaders
+    $LkupValue = $res | ConvertTo-Json
+    $LkupValue
+    $resObj = $LkupValue | ConvertFrom-Json
+    $resObj
+    $propValue = $resObj.$Column
+    $propValue
+
+    echo "LookupValue=$propValue" >> $env:GITHUB_OUTPUT
+}
+catch {
+    $_
+}
+
+
+
+#echo "::set-output name=LookupValue::$TableName"
+#echo "LookupValue=SHELL01" >> $env:GITHUB_OUTPUT
+# Write-Host "TableName: $($TableName)"
+# Write-Host "Query: $($Query)"
+# Write-Host "ColumnName: $($Column)"
+# $TableName
+# $Query
+# $Column
+
+$env
+
+try {
+    $Uri = "https://api.github.com/repositories/672865066/environments/Staging/secrets"
+    $reqHeaders = @{
+        "Authorization"        = "Bearer $($GitHubToken)";
+        "Accept"               = "application/vnd.github+json";
+        "X-GitHub-Api-Version" = "2022-11-28"
+    }
+    $res = Invoke-RestMethod -Method GET -Uri $Uri -Headers $reqHeaders
+    $res
+}
+catch {
+    $_
+}
